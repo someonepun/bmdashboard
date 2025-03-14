e2677a35bc4cf269bdf3dd1dcb6eb66f7f2d65ee
someonepun <someonepun@gmail.com>
2025-03-14 16:07:51 +0545

    Check

--- src/components/orders/ActionButtons.tsx
+++ src/components/orders/ActionButtons.tsx
@@ -45,4 +45,3 @@ export const ActionButtons: React.FC<ActionButtonsProps> = ({ onQuickView, onPri
     </Box>
   )
 }
-
--- src/pages/OrderDetail.tsx
+++ src/pages/OrderDetail.tsx
@@ -125,4 +125,3 @@ export const OrderDetail: React.FC = () => {
     </Layout>
   )
 }
-
